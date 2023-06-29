6d68f7a5dd33353fe8ee2ea5cf1dcd42eeaf3b68
Vinicios Neves <vinny@macbook.lan>
2023-06-29 21:58:25 +0100

    fim da aula 2 + desafio do serviço de unidade federativa

---
+++ src/app/core/services/form-busca.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormBuscaService {
+
+  formBusca: FormGroup;
+
+  constructor() { 
+
+    this.formBusca = new FormGroup({
+      somenteIda: new FormControl(false)
+    })
+  }
+}
