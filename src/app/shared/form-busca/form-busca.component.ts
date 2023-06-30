737b763a420b13e96249b17aa77b73dd155c970a
Vinicios Neves <vinny@macbook.lan>
2023-06-30 15:18:34 +0100

    fim da aula 4 + desafios

--- src/app/shared/form-busca/form-busca.component.ts
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ModalComponent } from '../modal/modal.component';
 import { FormBuscaService } from 'src/app/core/services/form-busca.service';
 
 @Component({
@@ -9,12 +7,8 @@ import { FormBuscaService } from 'src/app/core/services/form-busca.service';
   styleUrls: ['./form-busca.component.scss']
 })
 export class FormBuscaComponent {
-  constructor(public dialog: MatDialog, 
+  constructor( 
     public formBuscaService : FormBuscaService) {}
 
-  openDialog() {
-    this.dialog.open(ModalComponent, {
-      width: '50%'
-    })
-  }
+
 }
